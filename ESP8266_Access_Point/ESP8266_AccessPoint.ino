4f670d5743f9cc6ac065714dc71b81e799bf0637
gfm16617 <gfm16617@gmail.com>
2021-05-11 13:52:38 -0600

    Add files via upload

---
+++ ESP8266_Access_Point/ESP8266_AccessPoint.ino
@@ -0,0 +1,15 @@
+#include <ESP8266WiFi.h>
+
+const char *ssid = "EngrEDU IoT";
+const char *password = "";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.softAP(ssid, password); // Start the access point
+}
+
+void loop() {
+}
+
+
+
