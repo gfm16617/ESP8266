9d1750cd538fcef7e911ea4b5740e3b5228ec19d
gfm16617 <gfm16617@gmail.com>
2021-05-11 13:55:09 -0600

    Add files via upload

---
+++ ESP8266_TCP_Client/ESP8266_TCP_Client.ino
@@ -0,0 +1,37 @@
+#include <ESP8266WiFi.h>
+
+const char *ssid = "EngrEDU IoT"; 
+const char *password = "";
+
+const char* host = "192.168.4.2";
+const uint16_t port = 3000;
+
+// TCP socket
+WiFiClient client;
+
+int counter = 0;
+
+void setup() {
+  Serial.begin(115200);
+  // Create an Access Point
+  WiFi.softAP(ssid, password); // Start the access point
+}
+
+void loop() {
+
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    Serial.println("wait 2 sec...");
+    delay(2000);
+    return;
+  }
+
+  // Send TCP packet
+  client.println(counter++);
+  // Close connection
+  client.stop();
+
+  // Wait 1 sec
+  delay(1000);
+}
+
